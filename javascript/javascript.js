70fbd5931a06a90b870bad8ddef824c532dd5e4b
Krzysztof Jerszyński <krzysztof.jerszynski@o2.pl>
2021-06-02 00:15:10 +0200

    Added JavaScript code with option to add new task and display it

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,7 +15,9 @@
 
     for (const task of tasks) {
       htmlString += `
-          <li>
+          <li class="tasksList__item${
+            task.done ? " tasksList__item--done" : ""
+          }">
           ${task.content}
           </li>
           `;
@@ -23,8 +25,31 @@
     document.querySelector(".js-taskList").innerHTML = htmlString;
   };
 
+  const addNewTask = (newTaskItem) => {
+    tasks.push({
+      content: newTaskItem,
+    });
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskItem = document.querySelector(".js-input").value.trim();
+
+    if (newTaskItem === "") {
+      return;
+    }
+
+    addNewTask(newTaskItem);
+  };
+
   const init = () => {
     render();
+
+    const newTaskField__form = document.querySelector(".js-form");
+
+    newTaskField__form.addEventListener("submit", onFormSubmit);
   };
 
   init();
